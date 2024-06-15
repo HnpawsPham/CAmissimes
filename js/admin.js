7a276f9b2d62c9491393c1fa8e76aae9d0b2426c
HnpawsPham <hanpham123th@gmail.com>
2024-06-16 01:11:18 +0700

    fix v4

--- js/admin.js
+++ js/admin.js
@@ -45,139 +45,151 @@ function viewUserWork(account, workID) {
 adminName.innerHTML = "Admin: " + accountList[accountID].name;
 
 // LOAD ALL ACCOUNTS ON TABLE
-for (let [i, account] of accountList.entries()) {
-    let tr = document.createElement("tr");
+function loadAccounts() {
+    for (let [i, account] of accountList.entries()) {
+        let tr = document.createElement("tr");
 
-    let index = document.createElement("td");
-    index.innerHTML = account.index;
-    tr.appendChild(index);
+        let index = document.createElement("td");
+        index.innerHTML = account.index;
+        tr.appendChild(index);
 
-    let name = document.createElement("td");
+        let name = document.createElement("td");
 
-    if (account.name != undefined) {
-        name.innerHTML = account.name;
-    }
-    else {
-        name.innerHTML = "This account doesn't set a name yet";
-    }
-
-    tr.appendChild(name);
-
-    let email = document.createElement("td");
-    email.innerHTML = account.email;
-    tr.appendChild(email);
-
-    let pass = document.createElement("td");
-    pass.innerHTML = account.pass;
-    tr.appendChild(pass);
-
-    let workList = document.createElement("td");
-
-    if (account.work == undefined) {
-        let none = document.createElement("td");
-        none.innerHTML = "None";
-
-        tr.appendChild(none);
-    }
-    else {
-        for (let i in account.work) {
-
-            let card = document.createElement("div");
-            card.style.display = "flex";
-            card.style.justifyContent = "space-evenly";
-
-            let name = document.createElement("p");
-            name.style.textDecoration = "underline";
-            name.style.width = "30%";
-
-            if(account.work[i].name.length < 10){
-                name.innerHTML = account.work[i].name;
-            }
-            else{
-                name.innerHTML = account.work[i].name.substring(0,7) + "...";
-            }
-            
-            name.style.cursor = "pointer";
-            card.appendChild(name);
-
-            name.addEventListener("click", function () {
-                viewUserWork(account, i);
-            })
-
-            let checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-            checkbox.checked = account.work[i].isVerified;
-            card.appendChild(checkbox);
-
-            checkbox.addEventListener("change", function () {
-                if (checkbox.checked) {
-                    account.work[i]["isVerified"] = true;
-                    console.log(allUserWork[account.work[i].indexInAll].isVerified)
-                    allUserWork[account.work[i].indexInAll].isVerified = true;
-                }
-                else {
-                    account.work[i]["isVerified"] = false;
-                    allUserWork[account.work[i].indexInAll].isVerified = false;
-                }
-
-                saveToStorage("accountList", accountList);
-                saveToStorage("allUserWork", allUserWork);
-            })
-
-            let deleteBtn = document.createElement("button");
-            deleteBtn.style.width = "50%";
-            deleteBtn.style.height = "20%";
-            deleteBtn.innerHTML = "Delete experiment";
-            card.appendChild(deleteBtn);
-
-            deleteBtn.onclick = function () {
-                allUserWork.splice(account.work[i].indexInAll,1);
-                account.work.splice(i, 1);
-
-                saveToStorage("allUserWork", allUserWork);
-                saveToStorage("accountList", accountList);
-
-                delay(3000);
-                location.reload();
-            }
-
-            workList.appendChild(card);
-        }
-        tr.appendChild(workList);
-    }
-
-    let div = document.createElement("td");
-
-    let role = document.createElement("input");
-    role.type = "number";
-    role.min = 0;
-    role.max = 1;
-    role.value = account.role;
-
-    role.addEventListener("change", async function () {
-        account.role = role.value;
-
-        accountList[i] = account;
-        saveToStorage("accountList", accountList).then(
-            accountList = await findData("accountList")
-        );
-
-        await delay(3000);
-
-        if (accountList[accountID].role == 0) {
-            window.location.href = "../index.html"
+        if (account.name != undefined) {
+            name.innerHTML = account.name;
         }
         else {
-            alert("Change role successfully");
+            name.innerHTML = "This account doesn't set a name yet";
         }
-    })
+
+        tr.appendChild(name);
+
+        let email = document.createElement("td");
+        email.innerHTML = account.email;
+        tr.appendChild(email);
+
+        let pass = document.createElement("td");
+        pass.innerHTML = account.pass;
+        tr.appendChild(pass);
+
+        let workList = document.createElement("td");
+
+        if (account.work == undefined) {
+            let none = document.createElement("td");
+            none.innerHTML = "None";
+
+            tr.appendChild(none);
+        }
+        else {
+            for (let i in account.work) {
+
+                let card = document.createElement("div");
+                card.style.display = "flex";
+                card.style.justifyContent = "space-evenly";
+
+                let name = document.createElement("p");
+                name.style.textDecoration = "underline";
+                name.style.width = "30%";
+
+                if (account.work[i].name.length < 10) {
+                    name.innerHTML = account.work[i].name;
+                }
+                else {
+                    name.innerHTML = account.work[i].name.substring(0, 7) + "...";
+                }
+
+                name.style.cursor = "pointer";
+                card.appendChild(name);
+
+                name.addEventListener("click", function () {
+                    viewUserWork(account, i);
+                })
+
+                let checkbox = document.createElement("input");
+                checkbox.type = "checkbox";
+                checkbox.checked = account.work[i].isVerified;
+                card.appendChild(checkbox);
+
+                checkbox.addEventListener("change", function () {
+                    if (checkbox.checked) {
+                        account.work[i]["isVerified"] = true;
+                        console.log(allUserWork[account.work[i].indexInAll].isVerified)
+                        allUserWork[account.work[i].indexInAll].isVerified = true;
+                    }
+                    else {
+                        account.work[i]["isVerified"] = false;
+                        allUserWork[account.work[i].indexInAll].isVerified = false;
+                    }
+
+                    saveToStorage("accountList", accountList);
+                    saveToStorage("allUserWork", allUserWork);
+                })
+
+                let deleteBtn = document.createElement("button");
+                deleteBtn.style.width = "50%";
+                deleteBtn.style.height = "20%";
+                deleteBtn.innerHTML = "Delete experiment";
+                card.appendChild(deleteBtn);
+
+                deleteBtn.onclick = function () {
+                    allUserWork.splice(account.work[i].indexInAll, 1);
+                    account.work.splice(i, 1);
+
+                    saveToStorage("allUserWork", allUserWork);
+                    saveToStorage("accountList", accountList);
+
+                    delay(3000);
+                    table.replaceChildren();
+                    table.innerHTML = ` <tr>
+                                            <th>Index</th>
+                                            <th>Username</th>
+                                            <th>Email</th>
+                                            <th>Password</th>
+                                            <th>Worklist</th>
+                                            <th>Role</th>
+                                        </tr>`;
+                    loadAccounts();
+                }
+
+                workList.appendChild(card);
+            }
+            tr.appendChild(workList);
+        }
+
+        let div = document.createElement("td");
+
+        let role = document.createElement("input");
+        role.type = "number";
+        role.min = 0;
+        role.max = 1;
+        role.value = account.role;
+
+        role.addEventListener("change", async function () {
+            account.role = role.value;
+
+            accountList[i] = account;
+            saveToStorage("accountList", accountList).then(
+                accountList = await findData("accountList")
+            );
+
+            await delay(3000);
+
+            if (accountList[accountID].role == 0) {
+                window.location.href = "../index.html"
+            }
+            else {
+                alert("Change role successfully");
+            }
+        })
 
 
-    div.appendChild(role);
-    tr.appendChild(div);
+        div.appendChild(role);
+        tr.appendChild(div);
 
-    table.appendChild(tr);
+        table.appendChild(tr);
+    }
 }
+loadAccounts();
 
 // COMMENTS LIST
 function loadComments() {
