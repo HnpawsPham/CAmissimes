38742cd8c200d7ea647b8adb18a38dbed058a93b
HnpawsPham <hanpham123th@gmail.com>
2024-06-16 11:53:16 +0700

    add v3

--- js/login.js
+++ js/login.js
@@ -6,6 +6,7 @@ const form = document.getElementById("form");
 const emailInput = document.getElementById("email-input");
 const passInput = document.getElementById("pass-input");
 const notice = document.getElementById("notice");
+const forgetPass = document.getElementById("forget-pass");
 
 // DELAY
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
@@ -39,4 +40,29 @@ form.addEventListener("submit",function(e){
     }
 })
 
+// RECOVER PASSWORD
+forgetPass.addEventListener("click", function(e){
+    e.preventDefault();
 
+    if(emailInput.value.trim().length != 0){
+        let check = accountList.filter((account) => account.email == emailInput.value);
+
+        if(check.length > 0){
+            let params = {
+                from_email: emailInput.value,
+                email_password: check[0].pass
+            }
+
+            emailjs.send("service_m8mz1er", "template_alk8o0d", params)
+            .then(function(res){
+                visibleNotice("  We sent your password on your email account, check it  ");
+            })
+        }
+        else{
+            visibleNotice("There is no account that linked with this email");
+        }
+    }
+    else{
+        visibleNotice("Please enter your email first!");
+    }
+})
