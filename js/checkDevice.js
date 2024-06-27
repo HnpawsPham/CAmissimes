07abf5eac194599e5af2ca6fcf8ba002650071d9
HnpawsPham <hanpham123th@gmail.com>
2024-06-27 15:14:28 +0700

    add v7

--- js/checkDevice.js
+++ js/checkDevice.js
@@ -1,8 +1,12 @@
 // BLOCK OTHER DEVICES
 function blockMobile() {
-    if (window.innerWidth <= 992) {
-        alert("Chỉ hỗ trợ cho Laptop / Máy tính bàn hoặc để tab full màn hình")
+    if (window.innerWidth <= 1280) {
+        alert("Chỉ hỗ trợ PC hoặc để cửa sổ toàn màn hình!");
         location.reload()
     }
 }
 blockMobile();
+
+window.addEventListener("resize", function(){
+    blockMobile();
+});
