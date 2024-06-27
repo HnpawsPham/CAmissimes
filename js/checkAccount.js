375ffa991a2bb7870d2ea90feb889210a7cb06b6
HnpawsPham <hanpham123th@gmail.com>
2024-06-27 15:01:30 +0700

    add v7

--- js/checkAccount.js
+++ js/checkAccount.js
@@ -23,3 +23,12 @@ if(isLoggedIn){
         manage.classList.remove("hidden");
     }
 }
+
+// BLOCK OTHER DEVICES
+function blockMobile() {
+    if (window.innerWidth <= 992) {
+        alert("Chỉ hỗ trợ cho Laptop / Máy tính bàn hoặc để tab full màn hình")
+        location.reload()
+    }
+}
+blockMobile()
