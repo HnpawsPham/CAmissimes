322cc06e52d898bf41acdaf5fc6c51fe4880a64c
HnpawsPham <hanpham123th@gmail.com>
2024-09-14 22:10:28 +0700

    no-block

--- js/checkDevice.js
+++ js/checkDevice.js
@@ -1,12 +1,12 @@
 // BLOCK OTHER DEVICES
-function blockMobile() {
-    if (window.innerWidth <= 1280) {
-        alert("Support PC only! If you are using PC, please leave the window full screen.");
-        location.reload()
-    }
-}
-blockMobile();
+// function blockMobile() {
+//     if (window.innerWidth <= 1280) {
+//         alert("Support PC only! If you are using PC, please leave the window full screen.");
+//         location.reload()
+//     }
+// }
+// blockMobile();
 
-window.addEventListener("resize", function(){
-    blockMobile();
-});
+// window.addEventListener("resize", function(){
+//     blockMobile();
+// });
